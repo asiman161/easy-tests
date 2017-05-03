f21a6e7feb600712db460cd5fee07bf7cd27f323
Vlad Melnichenko <vlad.melnichenko96@gmail.com>
2017-05-03 18:04:43 +0300

    feat(styles):added styles for create-test

--- frontend/src/app/dashboard/create-test/variant/question/answers/answer.component.ts
+++ frontend/src/app/dashboard/create-test/variant/question/answers/answer.component.ts
@@ -6,17 +6,18 @@ import { FormGroup } from '@angular/forms';
   templateUrl: 'answer.component.html'
 })
 export class AnswerComponent {
-  @Input('group')
-  public answerForm: FormGroup;
-
-  @Input('answerIndex')
-  public answerIndex: number;
-
-  @Output()
-  public setAnswer: EventEmitter<any> = new EventEmitter();
+  @Input('group') public answerForm: FormGroup;
+  @Input('answerIndex') public answerIndex: number;
+  @Input('answersCount') public answersCount: number;
+  @Output() public setAnswer: EventEmitter<any> = new EventEmitter();
+  @Output() public removeAnswer: EventEmitter<any> = new EventEmitter();
 
 
   setAnswerIndex() {
     this.setAnswer.emit(this.answerIndex);
+  }
+
+  removeAnswerIndex(){
+    this.removeAnswer.emit(this.answerIndex);
   }
 }
